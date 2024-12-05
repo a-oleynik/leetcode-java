229058979c402d656710f0aac0b7865cef50ea05
Andrey Oleynik <andrei.oleynik@gmail.com>
2024-12-06 00:18:36 +0100

    Add solution to the Valid Anagram problem

---
+++ src/main/java/com/oleynik/java/leetcode/validanagram/Solution.java
@@ -0,0 +1,28 @@
+package com.oleynik.java.leetcode.validanagram;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            chars[t.charAt(i) - 'a']--;
+        }
+        for (int i : chars) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(new Solution().isAnagram(s, t));
+    }
+}
